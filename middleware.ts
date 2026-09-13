cb32593e23ee2a50f8378f53ba29d5cd562ecdfc
kate lint <ktlint@gmail.com>
2026-09-13 19:15:50 +0300

    Fix static assets matcher in middleware

--- middleware.ts
+++ middleware.ts
@@ -54,6 +54,6 @@ export const config = {
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      */
-    "/((?!_next/static|_next/image|favicon.ico).*)",
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp|css|js)$).*)",
   ],
 };
