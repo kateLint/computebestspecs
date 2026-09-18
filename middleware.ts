741977faf0cabdff7ff0d9ba4c037b0bdfa8a6a1
kate lint <ktlint@gmail.com>
2026-09-18 12:32:32 +0300

    fix(middleware): exclude txt, xml, and json static files from route matcher

--- middleware.ts
+++ middleware.ts
@@ -84,7 +84,8 @@ export const config = {
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
+     * - static asset extensions
      */
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp|css|js)$).*)",
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp|css|js|txt|xml|json)$).*)",
   ],
 };
